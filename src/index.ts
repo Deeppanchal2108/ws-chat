ee384eff39ccb3c34048a121b66be0d0db342dbb
Deeppanchal2108 <deepanchal2290@gmail.com>
2025-10-28 19:43:56 +0530

    Some normal 1 room logic

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { WebSocketServer } from "ws";
+import type WebSocket from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (socket) => {
-    
+
+let allSockets: WebSocket[] = [];
+wss.on("connection", (socket: WebSocket) => {
+    allSockets.push(socket);
     socket.on("message", (message) => {
         console.log("Received:", message.toString());
+        allSockets.forEach((socket)=>{socket.send(message.toString()+" from server")});
+
     });
     
 })
