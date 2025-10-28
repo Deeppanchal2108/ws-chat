547d48a010b53e6cd3573507cc7b706d4204c302
Deeppanchal2108 <deepanchal2290@gmail.com>
2025-10-28 19:29:58 +0530

    simple ws server

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (socket) => {
+    
+    socket.on("message", (message) => {
+        console.log("Received:", message.toString());
+    });
+    
+})
+
+console.log("WebSocket server is running on ws://localhost:8080");
