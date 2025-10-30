7ec9cd18816e8b4cc8c83ef45fb68f4f5f4f1d08
Deeppanchal2108 <deepanchal2290@gmail.com>
2025-10-30 11:18:56 +0530

    multiple rooms logic for chat

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,38 @@
 import { WebSocketServer } from "ws";
-import type WebSocket from "ws";
+import type { WebSocket} from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 
-let allSockets: WebSocket[] = [];
+interface User{
+    roomId: string;
+    socket: WebSocket;
+}
+
+let allSockets: User[] = [];
 wss.on("connection", (socket: WebSocket) => {
-    allSockets.push(socket);
     socket.on("message", (message) => {
-        console.log("Received:", message.toString());
-        allSockets.forEach((socket)=>{socket.send(message.toString()+" from server")});
+        const parsedMessage = JSON.parse(message.toString());
+
+        if(parsedMessage.type === "join"){
+            const {roomId} = parsedMessage;
+            allSockets.push({ roomId, socket: socket });
+            console.log(`A user joined room: ${roomId}`);
+        }
+
+        if (parsedMessage.type === "message") {
+            const recipients = allSockets.filter(user => user.roomId === parsedMessage.roomId);
+            recipients.forEach(user => {
+                if(user.socket !== socket){
+                    user.socket.send(JSON.stringify({
+                        type: "message",
+                        content: parsedMessage.text,
+                        roomId: parsedMessage.roomId
+                    }));
+                }
+            });
+
+        }
 
     });
     
