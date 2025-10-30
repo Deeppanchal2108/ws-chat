ea7b7be0ef39b707c8fb8418a1cc1284ba85c122
Deeppanchal2108 <deepanchal2290@gmail.com>
2025-10-30 14:33:38 +0530

    Add Redis integration for pub/sub messaging and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,93 @@
+// import { WebSocketServer } from "ws";
+// import type { WebSocket} from "ws";
+
+// const wss = new WebSocketServer({ port: 8080 });
+
+
+// interface User{
+//     roomId: string;
+//     socket: WebSocket;
+// }
+
+// let allSockets: User[] = [];
+// wss.on("connection", (socket: WebSocket) => {
+//     socket.on("message", (message) => {
+//         const parsedMessage = JSON.parse(message.toString());
+
+//         if(parsedMessage.type === "join"){
+//             const {roomId} = parsedMessage;
+//             allSockets.push({ roomId, socket: socket });
+//             console.log(`A user joined room: ${roomId}`);
+//         }
+
+//         if (parsedMessage.type === "message") {
+//             const recipients = allSockets.filter(user => user.roomId === parsedMessage.roomId);
+//             recipients.forEach(user => {
+//                 if(user.socket !== socket){
+//                     user.socket.send(JSON.stringify({
+//                         type: "message",
+//                         content: parsedMessage.text,
+//                         roomId: parsedMessage.roomId
+//                     }));
+//                 }
+//             });
+
+//         }
+
+//     });
+    
+// })
+
+// console.log("WebSocket server is running on ws://localhost:8080");
+
+
+
+
+// redis pub/sub logic
+
 import { WebSocketServer } from "ws";
-import type { WebSocket} from "ws";
+import type { WebSocket } from "ws";
+import Redis from "ioredis";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+const pub = new Redis();
+const sub = new Redis();
 
-interface User{
+interface User {
     roomId: string;
     socket: WebSocket;
 }
-
 let allSockets: User[] = [];
+
+sub.psubscribe("rooms:*");
+sub.on("pmessage", (_, channel, message) => {
+    const roomID = channel.split(":")[1];
+    const recipients = allSockets.filter(user => user.roomId === roomID);
+
+    recipients.forEach((user) => {
+        user.socket.send(message);
+    })
+    
+})
+
+
 wss.on("connection", (socket: WebSocket) => {
     socket.on("message", (message) => {
         const parsedMessage = JSON.parse(message.toString());
 
-        if(parsedMessage.type === "join"){
-            const {roomId} = parsedMessage;
+        if (parsedMessage.type === "join") {
+            const { roomId } = parsedMessage;
             allSockets.push({ roomId, socket: socket });
             console.log(`A user joined room: ${roomId}`);
         }
 
         if (parsedMessage.type === "message") {
-            const recipients = allSockets.filter(user => user.roomId === parsedMessage.roomId);
-            recipients.forEach(user => {
-                if(user.socket !== socket){
-                    user.socket.send(JSON.stringify({
-                        type: "message",
-                        content: parsedMessage.text,
-                        roomId: parsedMessage.roomId
-                    }));
-                }
-            });
-
+            const { roomId, text } = parsedMessage;
+            pub.publish(`rooms:${roomId}`, JSON.stringify({ type: "message", text }));
         }
 
     });
-    
 })
 
 console.log("WebSocket server is running on ws://localhost:8080");
